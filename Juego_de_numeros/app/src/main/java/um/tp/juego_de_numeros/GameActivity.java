4668490b7717ebac80298f17decf5ce1f2e376bb
Brenda Avalos <46949624+breenavalos@users.noreply.github.com>
2022-12-05 22:21:22 -0300

    Creación de layout

---
+++ Juego_de_numeros/app/src/main/java/um/tp/juego_de_numeros/GameActivity.java
@@ -0,0 +1,14 @@
+package um.tp.juego_de_numeros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
---
+++ Juego_de_numeros/app/src/main/java/um/tp/juego_de_numeros/MainActivity.java
@@ -0,0 +1,42 @@
+package um.tp.juego_de_numeros;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import um.tp.juego_de_numeros.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private Button buttonStartGame;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        buttonStartGame=findViewById(R.id.button_start_game);
+        buttonStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,GameActivity.class));
+            }
+        });
+    }
+}
